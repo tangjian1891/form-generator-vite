d8e3e56f4b3778e4559a26be87bebd5abc2bf570
tangjian1891 <tangjian1891@163.com>
2021-10-21 09:26:21 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,13 @@
   <!-- <handle></handle> -->
   <!-- <Nested></Nested> -->
   <!-- 测试JSX写法 -->
-  <!-- <TestJSX></TestJSX> -->
   <!-- <Test2></Test2> -->
-  <!-- <Left class="left"></Left>
-    <Middle class="middle"></Middle>-->
   <AppMain></AppMain>
 </template>
 
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import Left from "./components/Left.vue";
-import Middle from "./views/Middle.vue";
 import Simple from "./components/draggable-demo/01Simple.vue";
 import TwoLists from "./components/draggable-demo/02TwoLists.vue";
 import Clone from "./components/draggable-demo/03Clone.vue";
@@ -26,7 +21,6 @@ import CustomClone from "./components/draggable-demo/04CustomClone.vue";
 import CloneOnControl from "./components/draggable-demo/05CloneOnControl.vue";
 import handle from "./components/draggable-demo/06Hadnle.vue";
 import Nested from "./components/draggable-demo/07Nested.vue";
-import TestJSX from "./components/TestJSX.vue";
 import AppMain from "./views/AppMain.vue";
 </script>
 
